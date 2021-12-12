de18526140fb92b947887986de6988b2ed77b884
mmelendez2 <melendez5113@gmail.com>
2021-12-11 20:34:45 -0500

    Analyzing Weather Data using SQLite and Flask

---
+++ app.py
@@ -0,0 +1,40 @@
+# app route - C:\Users\melen\OneDrive\Documents\Bootcamp_Challenges\Module 9 - SQL Lite\Surfs_Up\app.py
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
